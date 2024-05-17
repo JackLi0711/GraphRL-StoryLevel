make adjustment about Record-related code / correct the codes in structure.py, include: 1. determination of node features for static graph, 2. unit conversion of Zz value for neighbor members connected to nodes / add opensees.py in order to replace pisa as static analysis tool

RL/new_strategy.py
@@ -84,8 +84,8 @@ def strong_column_weak_beam_driven_action(structure: Structure, fail_conditions:
         story_beam_update_conditions[story-1, 1] = 1
     #print(f"{story_beam_update_conditions = }")
 
-    xdir_beam_update_action = sorted(list(np.where(story_beam_update_conditions[:, 0] == 1)[0]), reverse=True)
-    zdir_beam_update_action = sorted(list(np.where(story_beam_update_conditions[:, 1] == 1)[0] + structure.story_num), reverse=True)
+    xdir_beam_update_action = sorted(np.where(story_beam_update_conditions[:, 0] == 1)[0].tolist(), reverse=True)
+    zdir_beam_update_action = sorted((np.where(story_beam_update_conditions[:, 1] == 1)[0] + structure.story_num).tolist(), reverse=True)
 
     return xdir_beam_update_action, zdir_beam_update_action
 

Structure/load.py
@@ -93,9 +93,12 @@ class NodalLoad:
         self.E_y *= (1.4 * Fu)
 
     def calculate_nodal_load(self, structure) -> Tuple[np.array, np.array, np.array]:
-        # (1)D, (2)L, (3)Ex and Ez, (4)Ey
-        # vertical load: (1), (2), (4)
-        # horizontal load: (3)
+        """
+        * vertical load: self-weight, D, L, Ey
+        * horizontal load: Ex or Ez
+        
+        return: nodal_vertical_load, nodal_horizontal_x_load, nodal_horizontal_z_load
+        """
         nodal_vertical_load = self._distribute_vertical_load(structure)
         nodal_horizontal_x_load, nodal_horizontal_z_load = self._distribute_horizontal_load(structure)
         return nodal_vertical_load, nodal_horizontal_x_load, nodal_horizontal_z_load  # kN

train.py
@@ -123,7 +123,8 @@ def main(args):
 
 	# set device
 	device = "cuda" if torch.cuda.is_available() else "cpu"
-	logger.critical(f"Device: {torch.cuda.get_device_name(device) if device == "cuda" else "CPU"}")
+	device_name = torch.cuda.get_device_name(device) if device == "cuda" else "CPU"
+	logger.critical(f"Device: {device_name}")
 
 	# setupt nonliear dynamic analysis simulator
 	nda_simulator = None
@@ -214,7 +215,7 @@ def main(args):
 	_train_kwargs = {
 		"agent": double_dqn_agent,
 		"env": env,
-		"record": rec,
+		"rec": rec,
 		"number_episodes": args.num_epoch,
 		"logger": logger,
 	}
@@ -227,11 +228,11 @@ def main(args):
 	# logger.critical(f"Testing Final Design: {test_info['test_final_design']}\n\n\n")
 	# logger.critical(f"Testing Final Material Usage: {test_info['test_final_material_usage']}\n\n\n")
 
-	# best_performance = min(test_info["test_final_material_usage"])
-	# best_episode = np.argmin(np.array(test_info["test_final_material_usage"]))
-	# best_design = test_info["test_final_design"][best_episode]
-	# logger.critical(f"Minimum Material Usage: {best_performance:.3f} m3")
-	# logger.critical(f"Best Story Level Sections: {best_design}")
+	best_episode = np.argmin(rec.testing_record["final_volume"])
+	best_performance = rec.testing_record["final_volume"][best_episode]
+	best_design = rec.testing_record["final_design"][best_episode]
+	logger.critical(f"Minimum Material Usage: {best_performance:.3f} m3")
+	logger.critical(f"Best Story Level Sections: {best_design}")
 
 	plot.plot_reward(rec, args.ckpt_dir)
 	plot.plot_loss(rec.learn_losses, args.ckpt_dir)
@@ -246,7 +247,6 @@ def main(args):
 	#visualize.visualize_edge_embedding(double_dqn_agent, env, logger, args.ckpt_dir)
 
 	# output record
-	rec.output(args.ckpt_dir)
 	# with open(args.ckpt_dir / "score_info.txt", "w") as f: json.dump(score_info, f)
 	# with open(args.ckpt_dir / "fail_info.txt", "w") as f: json.dump(fail_info, f)
 	# with open(args.ckpt_dir / "test_info.txt", "w") as f: json.dump(test_info, f)