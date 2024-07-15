add static structural response to graph features / revise the calculation for the distributed area of nodes (only influence fixed, small_random shape) / can store learn_losses.npy, Q_values.npy

RL/agent.py
@@ -200,7 +200,7 @@ class DeepQAgent(Agent):
         return action, q_val
     
     
-    def _learn(self, experiences: np.array) -> None:
+    def _learn(self, experiences: np.ndarray) -> None:
         """Update the agent's Q network based on a collection of prioritized experiences."""
         graphs, actions, rewards, next_graphs, dones, auxs = [vs for vs in zip(*experiences)]
 
@@ -346,8 +346,7 @@ def _train_an_episode(agent: DeepQAgent,
         action, q_val = agent.choose_action(state, 
                                             structure.already_minimum_section_story_indexes,
                                             dont_select_story_member_indexes)
-        print(f"\n-----episode: {agent._number_episodes+1:4d}, timestep: {agent._number_timesteps+1:3d}, story_level_sections: {structure.story_level_sections}")
-        print(f"-----episode: {agent._number_episodes+1:4d}, timestep: {agent._number_timesteps+1:3d}, action: {action:3d}")
+        print(f"\n-----episode: {agent._number_episodes+1:4d}, timestep: {agent._number_timesteps+1:3d}, story_level_sections: {structure.story_level_sections}, action: {action:3d}")
         structure, reward, done, fail_name, fail_reason = env.step(structure, action)
 
         # record
@@ -391,7 +390,6 @@ def _testing(agent: DeepQAgent,
     graph = structure.graph.clone()
     score = 0
     timestep = 0
-    actions = []
     done = False
     while not done:
         original_structure = deepcopy(structure)
@@ -405,7 +403,7 @@ def _testing(agent: DeepQAgent,
                                         structure.already_minimum_section_story_indexes,
                                         dont_select_story_member_indexes, 
                                         greedy=True)
-        print(f"\n*****Testing Episode, story_level_sections: {structure.story_level_sections}")
+        print(f"\n*****Testing Episode, story_level_sections: {structure.story_level_sections}, action: {action:3d}")
         structure, reward, done, fail_name, fail_reason = env.step(structure, action)
 
         # get next state
@@ -414,7 +412,6 @@ def _testing(agent: DeepQAgent,
         # record
         score += reward
         timestep += 1
-        actions.append(action)
         logger.info(f"*****Testing Episode, timestep: {timestep:3d}, action: {action:3d}, reward: {reward:4f},  acculmulate_score: {score:.4f} [ORIGINAL]")
 
         if env.saved_material_record_SCWB[-1] != 0:

RL/environment.py
@@ -4,20 +4,18 @@ import typing
 import logging
 import numpy as np
 
-from typing import List, Dict
 from pathlib import Path
 from copy import deepcopy
 
-from Structure import check
-from Structure import check_nda
-from Structure import structure
 from RL import new_strategy
+from Structure import structure, check, check_nda
 
 
 class Environment:
     def __init__(self, 
                  structure_shape: str,
                  add_structure_geometry: bool,
+                 add_response_features: bool,
                  reward_type: str,
                  scwb_driven_design: bool,
                  do_nonlinear_dynamic_analysis: bool,
@@ -25,14 +23,15 @@ class Environment:
                  check_displacement: bool,
                  nda_simulator: torch.nn.Module,
                  nda_norm_dict: dict,
-                 DBE_ground_motion_set: List[torch.Tensor],
-                 MCE_ground_motion_set: List[torch.Tensor],
+                 DBE_ground_motion_set: list[torch.Tensor],
+                 MCE_ground_motion_set: list[torch.Tensor],
                  checkpoint_dir: Path,
                  logger: logging.Logger,
-                 device = torch.device) -> None:
+                 device: torch.device) -> None:
         
         self.structure_shape = structure_shape
         self.add_structure_geometry = add_structure_geometry
+        self.add_response_features = add_response_features
         self.reward_type = reward_type
         self.scwb_driven_design = scwb_driven_design
         self.do_nonlinear_dynamic_analysis = do_nonlinear_dynamic_analysis
@@ -68,6 +67,8 @@ class Environment:
             z_span_num = 3
             x_span_lens = [7000, 11000, 14000]
             z_span_lens = [12000, 8000, 10000]
+            x_span_len = sum(x_span_lens) / len(x_span_lens)
+            z_span_len = sum(z_span_lens) / len(z_span_lens)
             story_num = 3
             story_height = 3500
         elif self.structure_shape == "random":
@@ -86,6 +87,7 @@ class Environment:
                                           "story_num": story_num, "story_height": story_height,
                                           "story_level_sections": story_level_sections, 
                                           "add_structure_geometry": self.add_structure_geometry, 
+                                          "add_response_features": self.add_response_features, 
                                           "do_nonlinear_dynamic_analysis": self.do_nonlinear_dynamic_analysis,
                                           "nda_norm_dict": self.nda_norm_dict,
                                           "analysis_dir": self.modal_analysis_dir}
@@ -99,7 +101,7 @@ class Environment:
 
 
     def init_check_setting(self, check_acc: bool, check_disp: bool):
-        """Reset whether to check the constraints according to the reward type"""
+        """Reset whether to check the constraints according to the reward type."""
         self.check_acceleration = check_acc
         self.check_displacement = check_disp
         if 'acceleration' in self.reward_type:
@@ -109,7 +111,7 @@ class Environment:
 
 
     def init_records(self, structure: structure.Structure):
-        """Initialize various records corresponding to different reward types"""
+        """Initialize various records corresponding to different reward types."""
         # material usage
         self.saved_material_record = []
         self.saved_material_record_SCWB = []
@@ -121,8 +123,9 @@ class Environment:
 
         # static response: max stress ratio, min stress ratio, max drift ratio, min SCWB ratio (all normalized by limit)
         _, load_cases, responses = check.get_response(structure, self.code_analysis_dir)
-        _, static_response = check.check_response(structure, load_cases, responses)
-        self.static_response_record = [static_response.tolist()]
+        _, static_response_features, static_response_rewards = check.process_response(structure, load_cases, responses)
+        structure._init_graph(static_response_features)
+        self.static_response_record = [list(static_response_rewards.values())]
 
         # dynamic response: acc, disp
         if self.do_nonlinear_dynamic_analysis and "acceleration" in self.reward_type:
@@ -162,6 +165,8 @@ class Environment:
                 z_span_num = 3
                 x_span_lens = [7000, 11000, 14000]
                 z_span_lens = [12000, 8000, 10000]
+                x_span_len = sum(x_span_lens) / len(x_span_lens)
+                z_span_len = sum(z_span_lens) / len(z_span_lens)
                 story_num = 3
                 story_height = np.random.randint(0, 11) * 100 + 3000
 
@@ -170,6 +175,8 @@ class Environment:
                 z_span_num = np.random.randint(2, 5)
                 x_span_lens = [np.random.randint(5, 15) * 1000 for i in range(x_span_num)]
                 z_span_lens = [np.random.randint(5, 15) * 1000 for i in range(z_span_num)]
+                x_span_len = sum(x_span_lens) / len(x_span_lens)
+                z_span_len = sum(z_span_lens) / len(z_span_lens)
                 story_num = np.random.randint(2, 5)
                 story_height = np.random.randint(0, 11) * 100 + 3000
 
@@ -189,6 +196,7 @@ class Environment:
                             "story_num": story_num, "story_height": story_height,
                             "story_level_sections": story_level_sections, 
                             "add_structure_geometry": self.add_structure_geometry,
+                            "add_response_features": self.add_response_features,
                             "do_nonlinear_dynamic_analysis": self.do_nonlinear_dynamic_analysis, 
                             "nda_norm_dict": self.nda_norm_dict,
                             "analysis_dir": self.modal_analysis_dir}
@@ -214,10 +222,10 @@ class Environment:
         volume_saved = self.saved_material_record[-1]
         volume_saved_SCWB = self.saved_material_record_SCWB[-1]
         if "material" in self.reward_type:
-            print(f"saved_material_record len: {len(self.saved_material_record)}")
-            print(f"{volume_saved = :.3f} m3")
-            print(f"{volume_saved_SCWB = :.3f} m3")
-            print(f"material usage difference: {(self.material_usage_record[-2] - self.material_usage_record[-1]):.3f} m3")
+            # print(f"saved_material_record len: {len(self.saved_material_record)}")
+            # print(f"{volume_saved = :.3f} m3")
+            # print(f"{volume_saved_SCWB = :.3f} m3")
+            # print(f"material usage difference: {(self.material_usage_record[-2] - self.material_usage_record[-1]):.3f} m3")
             
             reward += volume_saved
             if "total" in self.reward_type: reward += volume_saved_SCWB
@@ -272,11 +280,12 @@ class Environment:
             update_actions_SCWB = []
             auxiliary_values, load_cases, responses = check.get_response(structure, self.code_analysis_dir)
         
-        # 3. linear static analysis: check if response pass constraints
-        constraint_condition, static_response = check.check_response(structure, load_cases, responses)
+        # 2-1. linear static analysis: check if response pass constraints
+        constraint_condition, static_response_features, static_response_rewards = check.process_response(structure, load_cases, responses)
         whether_pass, fail_name, fail_reason = check.check_pass(load_cases, constraint_condition, self.check_displacement)
+        structure.update_garph(static_response_features)
 
-        # 4. nonlinear dynamic analysis: check if response pass constraints
+        # 2-2. nonlinear dynamic analysis: check if response pass constraints
         if self.do_nonlinear_dynamic_analysis and whether_pass == True:
             whether_pass, fail_reason = check_nda.check(structure, 
                                                         self.nda_simulator, 
@@ -289,7 +298,7 @@ class Environment:
                                                         self.device,
                                                         self.logger)
             
-        # 5. record all information after updating and checking
+        # 3. record all information after updating and checking
         # material usage
         self.saved_material_record.append(material_saved)
         self.saved_material_record_SCWB.append(material_saved_SCWB)
@@ -298,7 +307,7 @@ class Environment:
         self.update_actions_record.append(action)
         self.update_actions_record_SCWB.append(update_actions_SCWB)
         # static response
-        self.static_response_record.append(static_response.tolist())
+        self.static_response_record.append(list(static_response_rewards.values()))
         # dynamic response
         if self.do_nonlinear_dynamic_analysis and "acceleration" in self.reward_type:
             self.acc_record = check_nda.record_acc(structure, 
@@ -309,10 +318,10 @@ class Environment:
                                                    self.acc_record,
                                                    self.logger)
         
-        # 6. calculate reward based on recorded information
+        # 4. calculate reward based on recorded information
         reward = self.calculate_reward()
 
-        # 7. make proper adjustments if structure meets terminal state
+        # 5. make proper adjustments if structure meets terminal state
         if whether_pass == False:
             # fail constraints
             done = True
@@ -340,6 +349,6 @@ class Environment:
 
         return structure, reward, done, fail_name, fail_reason
         
-        
-        
+
+
 

RL/new_strategy.py
@@ -43,18 +43,17 @@ def sample_initial_story_sections(x_span_num: int, x_span_len: int,
 
 def check_strong_column_weak_beam(structure: Structure, responses: list[pisa.Response]) -> np.ndarray:
     fail_conditions = np.zeros((structure.node_number, 2))  # X-dir(0), Z-dir(1)
-    for response in responses:
-        # calculate strong-column-weak-beam ratio
-        Zc = structure.node_neighbor_Zz_matrix
+    for response in responses:        
         Puc = response.node_neighbor_Puc_matrix
         Puc[Puc > 0] = 0       # only consider compression case
         Ag = structure.node_neighbor_Ag_matrix
         Ag[Ag < 1e-5] = 1e+10  # in case area = 0 for faces not connected with members
-        
         reduced_stress = torch.abs(Puc / Ag)
-        ZcFyc =  (Zc * (YIELDING_STRESS - reduced_stress)) @ np.array([0, 0, 1, 1, 0, 0])               # [node_number, 1]
-        ZbFyb_x = (YIELDING_STRESS * structure.node_neighbor_Zz_matrix) @ np.array([1, 1, 0, 0, 0, 0])  # [node_number, 1]
-        ZbFyb_z = (YIELDING_STRESS * structure.node_neighbor_Zz_matrix) @ np.array([0, 0, 0, 0, 1, 1])  # [node_number, 1]
+
+        Zz = structure.node_neighbor_Zz_matrix
+        ZcFyc =  (Zz * (YIELDING_STRESS - reduced_stress)) @ np.array([0, 0, 1, 1, 0, 0])  # [node_number, 1]
+        ZbFyb_x = (YIELDING_STRESS * Zz) @ np.array([1, 1, 0, 0, 0, 0])                    # [node_number, 1]
+        ZbFyb_z = (YIELDING_STRESS * Zz) @ np.array([0, 0, 0, 0, 1, 1])                    # [node_number, 1]
         
         ratio_x = (ZcFyc / (ZbFyb_x + 1e-6)).squeeze()  # [node_number]
         ratio_z = (ZcFyc / (ZbFyb_z + 1e-6)).squeeze()  # [node_number]

Structure/opensees.py
@@ -15,7 +15,7 @@ m = 1e+3 * mm
 N = 1e-3 * kN
 Pa = N / m**2
 
-E = 200 * 1e+9 * Pa  # Young's modulud, GPa
+E = 200 * 1e+9 * Pa  # Young's modulus, GPa
 Nu = 0.3  # Poisson's ratio
 G = E / (2 * (1 + Nu))  # Shear modulus, GPa
 
@@ -88,8 +88,8 @@ def run_static_analysis(structure: Structure, load_case: NodalLoad, analysis_dir
         member_sections = column_sections if structure.member_category_dict[member_name] == 'y' else beam_sections
         sections_index = structure.member_section_dict[member_name]
         section = member_sections[sections_index]
-        #J = section["J(cm4)"] * 1e+4  # torsional constant, mm^4
-        Jxx = (section["I_y(cm4)"] + section["I_z(cm4)"]) * 1e+4  # torsional moment of inertia, mm^4
+        J = section["J(cm4)"] * 1e+4  # torsional constant, mm^4
+        #J = (section["I_y(cm4)"] + section["I_z(cm4)"]) * 1e+4  # torsional moment of inertia, mm^4
         Iy = section["I_y(cm4)"] * 1e+4  # moment of inertia, mm^4
         Iz = section["I_z(cm4)"] * 1e+4  # moment of inertia, mm^4
 
@@ -100,7 +100,7 @@ def run_static_analysis(structure: Structure, load_case: NodalLoad, analysis_dir
         else:
             transfTag = ColumnTransfTag
             
-        ops.element('elasticBeamColumn', eleTag, nodeTag_1, nodeTag_2, Area, E, G, Jxx, Iy, Iz, transfTag)
+        ops.element('elasticBeamColumn', eleTag, nodeTag_1, nodeTag_2, Area, E, G, J, Iy, Iz, transfTag)
 
     # master node
     for story, y in enumerate(structure.y_grid):
@@ -114,7 +114,8 @@ def run_static_analysis(structure: Structure, load_case: NodalLoad, analysis_dir
             moment_inertia = 0
             for slave_name in structure.slave_node_dict[master_name]:
                 translational_mass += structure.node_translational_mass_dict[slave_name]  # kN / (mm/s^2)
-                moment_inertia += structure.node_inertia_dict[slave_name]  # kN / (mm/s^2) * mm^2
+                Rx, Ry, Rz = structure.node_inertia_dict[slave_name]  # kN / (mm/s^2) * mm^2
+                moment_inertia += Ry
             mass = [translational_mass, translational_mass, 0] + [0, 0, moment_inertia]
             ops.node(nodeTag, *crds, '-mass',*mass)
             ops.fix(nodeTag, *[0,0,1,1,1,0])
@@ -170,7 +171,9 @@ def run_static_analysis(structure: Structure, load_case: NodalLoad, analysis_dir
     force = np.array([ops.basicForce(i+1) for i in range(structure.member_number)])  # 0: axial force, 1: moment at i end, 2: moment at j end
     
     # 6.31.1 nodeRecorder command (https://openseespydoc.readthedocs.io/en/latest/src/nodeRecorder.html)
-    file_name = analysis_dir / f"{load_case.name}.txt"
-    ops.recorder('Node', '-file',file_name, '-time', '-node',*[i+1 for i in range(structure.node_number)] ,'-dof',1,2,3, 'disp')
+    file_name = str(analysis_dir / f"{load_case.name}.txt")
+    nodeTags = [i+1 for i in range(structure.node_number)]
+    dofs = [1,2,3]
+    ops.recorder('Node', '-file',file_name, '-time', '-node',*nodeTags ,'-dof',*dofs , 'disp')
     
     return disp, force

Structure/structure.py
@@ -59,10 +59,11 @@ class Structure:
                  z_span_num: int, z_span_lens: List[int], 
                  story_num: int, story_height: float,
                  story_level_sections: List[int]=None,
-                 analysis_dir: str=None,
                  add_structure_geometry=True, 
+                 add_response_features=True,
                  do_nonlinear_dynamic_analysis=False, 
-                 nda_norm_dict: Dict=None):
+                 nda_norm_dict: Dict=None, 
+                 analysis_dir: str=None):
         
         self.x_span_num = x_span_num
         self.x_span_lens = x_span_lens
@@ -72,17 +73,16 @@ class Structure:
         self.story_height = story_height
         self.story_height_1F = story_height + 1000
         self.story_level_sections = story_level_sections
-        self.analysis_dir = analysis_dir
         self.add_structure_geometry = add_structure_geometry
+        self.add_response_features = add_response_features
         self.do_nonlinear_dynamic_analysis = do_nonlinear_dynamic_analysis
         self.nda_norm_dict = nda_norm_dict
+        self.analysis_dir = analysis_dir
         
         self._structure_initialization()
-        self._init_graph()
-        # print("graph:", self.graph)
+        # self._init_graph()
         if do_nonlinear_dynamic_analysis:
             self._init_nda_graph()
-            # print("nda_graph", self.nda_graph)
 
     def __str__(self):
         description = f"Structure: x_span_num: {self.x_span_num}, z_span_num: {self.z_span_num}, story_num: {self.story_num}, "
@@ -338,7 +338,6 @@ class Structure:
                         node2_index = coord_to_nodeIndex_dict[coord2]
                         member_to_nodeIndex_dict[member_name] = [node1_index, node2_index, FACE_INDEX['x_p'], FACE_INDEX['x_n'], NODE_FEATURE_FACE_INDEX['x_p'], NODE_FEATURE_FACE_INDEX['x_n']]
                         member_beam_index_list.append(member_index)
-
                         
                         # assign member with same location (vertically)
                         beam_XZ = "_".join([str((x + x_next) / 2), str(z)])
@@ -451,10 +450,10 @@ class Structure:
             self.story_level_sections = self.story_xdir_beam_section + self.story_zdir_beam_section + self.story_outer_column_section + self.story_inner_column_section
         else:
             xdir_beam, zdir_beam, outer_column, inner_column = np.array_split(self.story_level_sections, 4)
-            self.story_xdir_beam_section = list(xdir_beam)
-            self.story_zdir_beam_section = list(zdir_beam)
-            self.story_outer_column_section = list(outer_column)
-            self.story_inner_column_section = list(inner_column)
+            self.story_xdir_beam_section = xdir_beam.tolist()
+            self.story_zdir_beam_section = zdir_beam.tolist()
+            self.story_outer_column_section = outer_column.tolist()
+            self.story_inner_column_section = inner_column.tolist()
 
         self.story_level_actions = self.story_xdir_beam_member + self.story_zdir_beam_member + self.story_outer_column_member + self.story_inner_column_member
         self.story_level_categories = ['xdir_beam' for _ in range(len(self.story_xdir_beam_member))] + ['zdir_beam' for _ in range(len(self.story_zdir_beam_member))] + ['outer_column' for _ in range(len(self.story_outer_column_member))] + ['inner_column' for _ in range(len(self.story_inner_column_member))]
@@ -581,9 +580,27 @@ class Structure:
         # given node x, y, z, return the area distrubte to node
         # node distributed area depends on only neighboring slab, so doesn't need to update when reducing sections
         if y == 0: return 0
-        width_x = self.x_grid[1]/2 if x == min(self.x_grid) or x == max(self.x_grid) else self.x_grid[1]
-        width_z = self.z_grid[1]/2 if z == min(self.z_grid) or z == max(self.z_grid) else self.z_grid[1]
+
+        x_grid_coord = self.x_grid.index(x)
+        if x_grid_coord == 0:
+            width_x = (self.x_grid[1] - self.x_grid[0]) / 2
+        elif x_grid_coord == len(self.x_grid) - 1:
+            width_x = (self.x_grid[-1] - self.x_grid[-2]) / 2
+        else:
+            width_x = (self.x_grid[x_grid_coord+1] - self.x_grid[x_grid_coord-1]) / 2
+        
+        z_grid_coord = self.z_grid.index(z)
+        if z_grid_coord == 0:
+            width_z = (self.z_grid[1] - self.z_grid[0]) / 2
+        elif z_grid_coord == len(self.z_grid) - 1:
+            width_z = (self.z_grid[-1] - self.z_grid[-2]) / 2
+        else:
+            width_z = (self.z_grid[z_grid_coord+1] - self.z_grid[z_grid_coord-1]) / 2
+
+        # width_x = self.x_grid[1]/2 if x == min(self.x_grid) or x == max(self.x_grid) else self.x_grid[1]
+        # width_z = self.z_grid[1]/2 if z == min(self.z_grid) or z == max(self.z_grid) else self.z_grid[1]
         area = width_x/1000 * width_z/1000  # m2
+
         return area
 
 
@@ -649,19 +666,19 @@ class Structure:
         # For simplicity, use the average span len instead of the variant individual span lengths
         x_span_len = sum(self.x_span_lens) / len(self.x_span_lens)
         z_span_len = sum(self.z_span_lens) / len(self.z_span_lens)
-        quarter_slab_mass = x_span_len/1000 * z_span_len/1000 * SLAB_THICKNESS * CONCRETE_DENSITY   # kg
+        slab_mass = x_span_len/1000 * z_span_len/1000 * SLAB_THICKNESS * CONCRETE_DENSITY   # kg
 
         # 1 kN = 1e+6 kg * 1 mm/s2 --> 1 kg = 1e-6 kN / (mm/s2)
         # E.g., mass = 300 kg, it will be represented as 3e-4 (kN / (mm/s2)) in PISA
-        quarter_slab_translational_mass = quarter_slab_mass / 1e+06  # kN / (mm/s2)
+        slab_translational_mass = slab_mass / 1e+06  # kN / (mm/s2)
             
-        quarter_slab_global_Ix = 1 / 12 * quarter_slab_translational_mass * ((SLAB_THICKNESS*1000) ** 2 + z_span_len ** 2) 
-        quarter_slab_global_Iy = 1 / 12 * quarter_slab_translational_mass * (x_span_len ** 2 + z_span_len ** 2)
-        quarter_slab_global_Iz = 1 / 12 * quarter_slab_translational_mass * ((SLAB_THICKNESS*1000) ** 2 + x_span_len ** 2)
+        slab_global_Ix = 1 / 12 * slab_translational_mass * ((SLAB_THICKNESS*1000) ** 2 + z_span_len ** 2) 
+        slab_global_Iy = 1 / 12 * slab_translational_mass * (x_span_len ** 2 + z_span_len ** 2)
+        slab_global_Iz = 1 / 12 * slab_translational_mass * ((SLAB_THICKNESS*1000) ** 2 + x_span_len ** 2)
         
-        Rx = (quarter_slab_number / 4) * quarter_slab_global_Ix
-        Ry = (quarter_slab_number / 4) * quarter_slab_global_Iy
-        Rz = (quarter_slab_number / 4) * quarter_slab_global_Iz
+        Rx = (quarter_slab_number / 4) * slab_global_Ix
+        Ry = (quarter_slab_number / 4) * slab_global_Iy
+        Rz = (quarter_slab_number / 4) * slab_global_Iz
 
         return Rx, Ry, Rz  # unit: kN / (mm/s2) * mm2
     
@@ -697,12 +714,16 @@ class Structure:
         return beta_x, beta_z
 
 
-    def _init_graph(self):
-        # new node feature (if fix, if top, if side, beta_x, beta_z)
+    def _init_graph(self, response_features: dict[str, torch.Tensor] = None):
+        # new node feature: if fix, if top, if side, beta_x, beta_z
         node_feature_num = 8 if self.add_structure_geometry else 5
         node_feature = torch.zeros(self.node_number, node_feature_num)
 
-        beta_x, beta_z = self._strongColumn_weakBeam_beta()
+        if self.add_response_features:
+            beta_x = torch.tanh(1.0 / response_features["min_SCWB_ratio_x"]) 
+            beta_z = torch.tanh(1.0 / response_features["min_SCWB_ratio_z"])
+        else: 
+            beta_x, beta_z = self._strongColumn_weakBeam_beta()
         node_feature[:, 3] = beta_x
         node_feature[:, 4] = beta_z
 
@@ -732,7 +753,8 @@ class Structure:
                             
         # edge feature:     is_col, is_beam, L, (A, Iz, Iy, Zz), (A', Iz', Iy', Zz')
         # new edge feature: is_col, is_beam, L, (A, Iz, Iy, Zz), (A', Iz', Iy', Zz'), tanh(stress_ratio), tanh(drift_ratio)
-        edge_feature = torch.zeros(self.member_number * 2, 11)
+        edge_feature_num = 13 if self.add_response_features else 11
+        edge_feature = torch.zeros(self.member_number * 2, edge_feature_num)
 
         for original_member_index in range(self.member_number):
             member_name = f"E{original_member_index+1}"
@@ -762,6 +784,10 @@ class Structure:
             edge_feature[member_index, 8] = Iz
             edge_feature[member_index, 9] = Iy
             edge_feature[member_index, 10] = Zz
+
+            if self.add_response_features:
+                edge_feature[member_index, 11] = torch.tanh(response_features["max_stress_ratio"][original_member_index])
+                edge_feature[member_index, 12] = torch.tanh(response_features["max_drift_ratio"][original_member_index])
 
             # add another direction of edge feature back
             edge_feature[member_index+1, :] = edge_feature[member_index, :]
@@ -805,8 +831,8 @@ class Structure:
                         #   story_outer_column_member=tuple(story_outer_column_member), 
                         #   story_inner_column_member=tuple(story_inner_column_member),
                         #   story_batch=story_batch.to(torch.int64))
-        # print("graph:", self.graph)
         self._normalize()
+        print("graph:", self.graph)
 
         
     def _init_nda_graph(self):
@@ -915,6 +941,7 @@ class Structure:
 
         self.nda_graph = Data(x=node_feature, y=None, edge_index=edge_index, edge_attr=edge_feature)
         self._nda_normalize()
+        print("nda_graph", self.nda_graph)
 
 
     def _normalize(self):
@@ -1016,10 +1043,10 @@ class Structure:
         
         # update story member section from story_level_sections
         xdir_beam, zdir_beam, outer_column, inner_column = np.array_split(self.story_level_sections, 4)
-        self.story_xdir_beam_section = list(xdir_beam)
-        self.story_zdir_beam_section = list(zdir_beam)
-        self.story_outer_column_section = list(outer_column)
-        self.story_inner_column_section = list(inner_column)
+        self.story_xdir_beam_section = xdir_beam.tolist()
+        self.story_zdir_beam_section = zdir_beam.tolist()
+        self.story_outer_column_section = outer_column.tolist()
+        self.story_inner_column_section = inner_column.tolist()
         #print(f"Before & After auto-correct are same: {update_member_names == correct_needed_members}")
         
         # update structure and graph's variables
@@ -1075,40 +1102,104 @@ class Structure:
             
             # graph update
             # edge feature
-            self.graph.edge_attr[member_index, 3:7] = self.graph.edge_attr[member_index, 7:11]
-            self.graph.edge_attr[member_index, 7] = A / GRAPH_NORM_DICT["A"]
-            self.graph.edge_attr[member_index, 8] = Iz / GRAPH_NORM_DICT["Iz"]
-            self.graph.edge_attr[member_index, 9] = Iy / GRAPH_NORM_DICT["Iy"]
-            self.graph.edge_attr[member_index, 10] = Zz / GRAPH_NORM_DICT["Zz"]
-            self.graph.edge_attr[member_index+1, :] = self.graph.edge_attr[member_index, :]
+            # self.graph.edge_attr[member_index, 3:7] = self.graph.edge_attr[member_index, 7:11]
+            # self.graph.edge_attr[member_index, 7] = A / GRAPH_NORM_DICT["A"]
+            # self.graph.edge_attr[member_index, 8] = Iz / GRAPH_NORM_DICT["Iz"]
+            # self.graph.edge_attr[member_index, 9] = Iy / GRAPH_NORM_DICT["Iy"]
+            # self.graph.edge_attr[member_index, 10] = Zz / GRAPH_NORM_DICT["Zz"]
+            # self.graph.edge_attr[member_index+1, :] = self.graph.edge_attr[member_index, :]
 
             # nda_graph update
-            if self.do_nonlinear_dynamic_analysis:
+            # if self.do_nonlinear_dynamic_analysis:
                 # node's member feature
-                self.nda_graph.x[node1_index, node_feature_face_index1+1] = My / self.nda_norm_dict["moment"]
-                self.nda_graph.x[node2_index, node_feature_face_index2+1] = My / self.nda_norm_dict["moment"]
+                # self.nda_graph.x[node1_index, node_feature_face_index1+1] = My / self.nda_norm_dict["moment"]
+                # self.nda_graph.x[node2_index, node_feature_face_index2+1] = My / self.nda_norm_dict["moment"]
                 # edge feature
-                self.nda_graph.edge_attr[member_index, 3] = My / self.nda_norm_dict["moment"]
-                self.nda_graph.edge_attr[member_index+1, :] = self.nda_graph.edge_attr[member_index, :]
+                # self.nda_graph.edge_attr[member_index, 3] = My / self.nda_norm_dict["moment"]
+                # self.nda_graph.edge_attr[member_index+1, :] = self.nda_graph.edge_attr[member_index, :]
             
 
         # graph beta, modal period, shape update
-        beta_x, beta_z = self._strongColumn_weakBeam_beta()
-        self.graph.x[:, 3] = beta_x
-        self.graph.x[:, 4] = beta_z
+        # beta_x, beta_z = self._strongColumn_weakBeam_beta()
+        # self.graph.x[:, 3] = beta_x
+        # self.graph.x[:, 4] = beta_z
 
         # also update nda_graph
-        if self.do_nonlinear_dynamic_analysis:
-            self.nda_graph.x[:, 9] = beta_x
-            self.nda_graph.x[:, 10] = beta_z
-            first_mode_period, second_mode_period, third_mode_period, node_first_mode_shape, node_second_mode_shape, node_third_mode_shape = pisa.dynamic_analysis_period(self, self.analysis_dir)
-            self.nda_graph.x[:, 11] = first_mode_period / self.nda_norm_dict["period"]
-            self.nda_graph.x[:, 12] = second_mode_period / self.nda_norm_dict["period"]
-            self.nda_graph.x[:, 13] = third_mode_period / self.nda_norm_dict["period"]
-            self.nda_graph.x[:, 14:17] = torch.tensor(node_first_mode_shape) / self.nda_norm_dict["modal_shape"]
-            self.nda_graph.x[:, 17:20] = torch.tensor(node_second_mode_shape) / self.nda_norm_dict["modal_shape"]
-            self.nda_graph.x[:, 20:23] = torch.tensor(node_third_mode_shape) / self.nda_norm_dict["modal_shape"]
+        # if self.do_nonlinear_dynamic_analysis:
+        #     self.nda_graph.x[:, 9] = beta_x
+        #     self.nda_graph.x[:, 10] = beta_z
+        #     first_mode_period, second_mode_period, third_mode_period, node_first_mode_shape, node_second_mode_shape, node_third_mode_shape = pisa.dynamic_analysis_period(self, self.analysis_dir)
+        #     self.nda_graph.x[:, 11] = first_mode_period / self.nda_norm_dict["period"]
+        #     self.nda_graph.x[:, 12] = second_mode_period / self.nda_norm_dict["period"]
+        #     self.nda_graph.x[:, 13] = third_mode_period / self.nda_norm_dict["period"]
+        #     self.nda_graph.x[:, 14:17] = torch.tensor(node_first_mode_shape) / self.nda_norm_dict["modal_shape"]
+        #     self.nda_graph.x[:, 17:20] = torch.tensor(node_second_mode_shape) / self.nda_norm_dict["modal_shape"]
+        #     self.nda_graph.x[:, 20:23] = torch.tensor(node_third_mode_shape) / self.nda_norm_dict["modal_shape"]
         
         return volume_saved
         
+
+    def update_garph(self, response_features: dict[str, torch.Tensor] = None):
+        # update node features
+        if self.add_response_features: 
+            beta_x = torch.tanh(1.0 / response_features["min_SCWB_ratio_x"])
+            beta_z = torch.tanh(1.0 / response_features["min_SCWB_ratio_z"])
+        else: 
+            beta_x, beta_z = self._strongColumn_weakBeam_beta()
+        self.graph.x[:, 3] = beta_x
+        self.graph.x[:, 4] = beta_z
+
+        # update edge features
+        for member_index in range(self.member_number):
+            self.graph.edge_attr[member_index*2, 3:7] = self.graph.edge_attr[member_index*2, 7:11]
+
+            member_name = f"E{member_index+1}"
+            section_index = self.member_section_dict[member_name]  # structure is updated, so section_index is updated
+            section_info = column_sections[section_index] if self.member_category_dict[member_name] == 'y' else beam_sections[section_index]
+            A = section_info["A(cm2)"]
+            Iz = section_info["I_z(cm4)"]
+            Iy = section_info["I_y(cm4)"]
+            Zz = section_info["Z_z(cm3)"]
+            self.graph.edge_attr[member_index*2, 7] = A / GRAPH_NORM_DICT["A"]
+            self.graph.edge_attr[member_index*2, 8] = Iz / GRAPH_NORM_DICT["Iz"]
+            self.graph.edge_attr[member_index*2, 9] = Iy / GRAPH_NORM_DICT["Iy"]
+            self.graph.edge_attr[member_index*2, 10] = Zz / GRAPH_NORM_DICT["Zz"]
+            if self.add_response_features:
+                self.graph.edge_attr[member_index*2, 11] = torch.tanh(response_features["max_stress_ratio"][member_index])
+                self.graph.edge_attr[member_index*2, 12] = torch.tanh(response_features["max_drift_ratio"][member_index])
+
+            self.graph.edge_attr[member_index*2+1, :] = self.graph.edge_attr[member_index*2, :]
+
+
+    def update_nda_grpah(self, response_features: dict[str, torch.Tensor] = None):
+        # update node features
+        beta_x, beta_z = self._strongColumn_weakBeam_beta()
+        self.nda_graph.x[:, 9] = beta_x
+        self.nda_graph.x[:, 10] = beta_z
+        first_mode_period, second_mode_period, third_mode_period, node_first_mode_shape, node_second_mode_shape, node_third_mode_shape = pisa.dynamic_analysis_period(self, self.analysis_dir)
+        self.nda_graph.x[:, 11] = first_mode_period / self.nda_norm_dict["period"]
+        self.nda_graph.x[:, 12] = second_mode_period / self.nda_norm_dict["period"]
+        self.nda_graph.x[:, 13] = third_mode_period / self.nda_norm_dict["period"]
+        self.nda_graph.x[:, 14:17] = torch.tensor(node_first_mode_shape) / self.nda_norm_dict["modal_shape"]
+        self.nda_graph.x[:, 17:20] = torch.tensor(node_second_mode_shape) / self.nda_norm_dict["modal_shape"]
+        self.nda_graph.x[:, 20:23] = torch.tensor(node_third_mode_shape) / self.nda_norm_dict["modal_shape"]
+
+        # update edge features
+        for member_index in range(self.member_number):
+            member_name = f"E{member_index+1}"
+            node1_index, node2_index, face_number1, face_number2, node_feature_face_index1, node_feature_face_index2 = self.member_to_nodeIndex_dict[member_name]
+            
+            section_index = self.member_section_dict[member_name]  # structure is updated, so section_index is updated
+            section_info = column_sections[section_index] if self.member_category_dict[member_name] == 'y' else beam_sections[section_index]
+            My = section_info["My_z(kN-mm)"]
+
+            # node's member feature
+            self.nda_graph.x[node1_index, node_feature_face_index1+1] = My / self.nda_norm_dict["moment"]
+            self.nda_graph.x[node2_index, node_feature_face_index2+1] = My / self.nda_norm_dict["moment"]
+            # edge feature
+            self.nda_graph.edge_attr[member_index*2, 3] = My / self.nda_norm_dict["moment"]
+            self.nda_graph.edge_attr[member_index*2+1, :] = self.nda_graph.edge_attr[member_index*2, :]
+
+
+
 

train.py
@@ -16,9 +16,7 @@ sys.path.append("Visualization")
 sys.path.append("NonlinearDynamicAnalysisSimulator/")
 
 from RL import agent, environment, record
-from RL.agent import train
-from Visualization import plot
-from Visualization import visualize
+from Visualization import plot, visualize
 from NonlinearDynamicAnalysisSimulator import load_simulator
 
 
@@ -34,7 +32,7 @@ def parse_args() -> Namespace:
 	#parser.add_argument("--ckpt_dir", type=Path, default="./Results/AccelerationReward/")
 
 	# suffix
-	parser.add_argument("--suffix", type=str, default="NewReward_RMSprop_RandomShape_FixedEpsilon0.1_BufferSize10000_BatchSize256_Test")  # material
+	parser.add_argument("--suffix", type=str, default="FixedShape_ResponseFeatures_RMSprop_EpsilonDecay099_BufferSize3000_BatchSize256_Epoch300")  # material
 	#parser.add_argument("--suffix", type=str, default="doNDA_NormalizedReward_RestrictAction_NoColStrength_Epoch300")  # acceleration
 
 	# nonlinear dynamic analysis simulator
@@ -46,9 +44,10 @@ def parse_args() -> Namespace:
 	parser.add_argument("--ground_motion_number", type=int, default=11, help="ASCE says 11 is better")
 
 	# structure
-	parser.add_argument("--structure_shape", type=str, default="random", help="fixed, small_random, random")
+	parser.add_argument("--structure_shape", type=str, default="fixed", help="fixed, small_random, random")
 	parser.add_argument("--add_structure_geometry", action="store_true", default=True)
-	parser.add_argument("--reward_type", type=str, default="combined", help="material, acceleration, displacement, normalized, total, combined")
+	parser.add_argument("--add_response_features", action="store_true", default=True)
+	parser.add_argument("--reward_type", type=str, default="material", help="material, acceleration, displacement, normalized, total, combined")
 	parser.add_argument("--restrict_action", action="store_true", default=False)
 	parser.add_argument("--scwb_driven_design", action="store_true", default=False)
 
@@ -57,7 +56,7 @@ def parse_args() -> Namespace:
 	parser.add_argument("--num_layers", type=int, default=3)
 
 	# buffer
-	parser.add_argument("--buffer_size", type=int, default=10000)
+	parser.add_argument("--buffer_size", type=int, default=3000)
 	parser.add_argument("--update_frequency", type=int, default=1)
 	parser.add_argument("--add_experience_frequency", type=int, default=1)
 
@@ -69,7 +68,7 @@ def parse_args() -> Namespace:
 	parser.add_argument("--test_frequency", type=int, default=5)
 	parser.add_argument("--batch_size", type=int, default=256)  # original: 256
 	parser.add_argument("--lr", type=float, default=1e-5)
-	parser.add_argument("--num_epoch", type=int, default=10, help="epoch == episode")
+	parser.add_argument("--num_epoch", type=int, default=300, help="epoch == episode")
 	parser.add_argument("--random_seed", type=int, default=731, help="fixed random seed")
 
 	args = parser.parse_args()
@@ -165,7 +164,7 @@ def main(args):
 
 	# Agent
 	node_feature_dim = 8 if args.add_structure_geometry else 5
-	edge_feature_dim = 11
+	edge_feature_dim = 13 if args.add_response_features else 11
 	_agent_kwargs = {
 		"node_feature_dim": node_feature_dim,
 		"edge_feature_dim": edge_feature_dim,
@@ -174,7 +173,7 @@ def main(args):
 		"batch_size": args.batch_size,
 		"lr": args.lr,
 		"buffer_size": args.buffer_size,
-		"epsilon_decay_schedule": fixed_epsilon_schedule,
+		"epsilon_decay_schedule": epsilon_decay_schedule,
 		"synchronize_steps": args.synchronize_steps,
 		"soft_update_alpha": args.soft_update_alpha,
 		"gamma": args.gamma,
@@ -185,7 +184,7 @@ def main(args):
 		"seed": args.random_seed,
 		"logger": logger,
 		"pretrained_ckpt_dir": args.pretrained_ckpt_dir,
-		"device":device,
+		"device": device,
 	}
 	double_dqn_agent = agent.DeepQAgent(**_agent_kwargs)
 
@@ -193,6 +192,7 @@ def main(args):
 	_env_kwargs = {
 		"structure_shape": args.structure_shape,
 		"add_structure_geometry": args.add_structure_geometry,
+		"add_response_features": args.add_response_features,
 		"reward_type": args.reward_type,
 		"scwb_driven_design": args.scwb_driven_design,
 		"do_nonlinear_dynamic_analysis": args.do_nonlinear_dynamic_analysis,
@@ -221,7 +221,7 @@ def main(args):
 	}
 
 
-	train(**_train_kwargs)
+	agent.train(**_train_kwargs)
 
 	logger.critical(f"Minimum Material Usage: {np.min(rec.testing_record['final_volume']):.3f} m3, Story Level Sections: {rec.testing_record['final_design'][np.argmin(rec.testing_record['final_volume'])]}")
 	logger.critical(f"Highest Score: {np.max(rec.testing_record['score']):.3f}, Story Level Sections: {rec.testing_record['final_design'][np.argmax(rec.testing_record['score'])]}")
@@ -244,5 +244,4 @@ def main(args):
 if __name__ == "__main__":
 	args = parse_args()
 	main(args)
-
 